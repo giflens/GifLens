7d394d9ae669029536dfd22cf0829e84cdf91210
Fabien Allanic <fabien.allanic@gmail.com>
2019-04-07 17:36:23 -0500

    installed axios, added utils function to searchgifs on Giphy, and added unit tests for that utility function

---
+++ src/utils.ts
@@ -0,0 +1,23 @@
+import { IncomingMessage } from 'http';
+
+const axios = require('axios');
+
+const GIPHY_API_KEY = 'VDTsjv7FD1PCrcZ5AtyhYMSPW2TREanK';
+
+/**
+ * Function to search a gif on Giphy.
+ * @param searchTerms A string with the search terms the user typed.
+ * @returns An array of fixed height (200px) gifs which will fit correctly in our Gif hover and preview popups.
+ */
+export const searchGif = async (searchTerms: string): Promise<string[]> => {
+	const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURIComponent(
+		searchTerms
+	)}&api_key=${GIPHY_API_KEY}`;
+
+	const response = await axios.get(url);
+	const data = response.data.data.map(
+		(imageObject: any) => imageObject.images.fixed_height.url
+	);
+
+	return data;
+};
