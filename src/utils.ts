33d11b4caf348c0ebc2dc9cf8ae59b16114da107
Maxime Burlat <maximeburlat@gmail.com>
2019-05-26 14:43:23 -0500

    updated searchGIF (API call) to take in page number

--- src/utils.ts
+++ src/utils.ts
@@ -7,10 +7,13 @@ const GIPHY_API_KEY = 'VDTsjv7FD1PCrcZ5AtyhYMSPW2TREanK';
  * @param searchTerms A string with the search terms the user typed.
  * @returns An array of fixed height (200px) gifs which will fit correctly in our Gif hover and preview popups.
  */
-export const searchGif = async (searchTerms: string): Promise<string[]> => {
+export const searchGif = async (
+	searchTerms: string,
+	pageNumber = 1
+): Promise<string[]> => {
 	const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURIComponent(
 		searchTerms
-	)}&api_key=${GIPHY_API_KEY}`;
+	)}&api_key=${GIPHY_API_KEY}&offset=${(pageNumber - 1) * 20}`;
 
 	const response = await axios.get(url);
 	const data = response.data.data.map(
