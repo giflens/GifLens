6c482dda20e7293283e37df6ca445d0f928882c2
Maxime Burlat <maximeburlat@gmail.com>
2019-06-21 11:05:55 -0500

    added a test for adding a GIFLENS tag to an editor (simulating with vscode api

--- src/test/addGif.test.ts
+++ src/test/addGif.test.ts
@@ -1,9 +1,10 @@
 import * as assert from 'assert';
-// import * as vscode from 'vscode';
+import * as vscode from 'vscode';
 
 import {
 	getLanguageCommentEnd,
 	getLanguageCommentStart,
+	addGifLensTagToEditor,
 	// searchTask,
 } from '../addGif';
 
@@ -16,5 +17,24 @@ suite('AddGif creates a GIFLENS tag', function() {
 	test('given a correct language identifier, getLanguageCommentEnd should return the correct comment end', function() {
 		const commentEnd = getLanguageCommentEnd('css');
 		assert(commentEnd === ' */');
+	});
+
+	test('given an editor and a non empty string, addGifLensTagToEditor correctly adds the GIFLENS tag', async function() {
+		const doc = await vscode.workspace.openTextDocument({
+			language: 'javascript',
+		});
+		const editor = await vscode.window.showTextDocument(doc);
+		assert(vscode.window.activeTextEditor === editor);
+		if (vscode.window.activeTextEditor) {
+			assert(doc === vscode.window.activeTextEditor.document);
+		}
+		const editStatus = await addGifLensTagToEditor(
+			vscode.window.activeTextEditor,
+			'test'
+		);
+		assert(editStatus);
+		if (editStatus) {
+			assert(doc.lineAt(0).text === '// GIFLENS-test');
+		}
 	});
 });
