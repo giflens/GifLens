bbae75cb6a410b27bf4879a54795de1888574c18
Maxime Burlat <maximeburlat@gmail.com>
2019-06-24 21:57:20 -0500

    corrected a bug on the webview, plus added the possibility to click the image to create a GIFLENS tag

--- src/viewGif.ts
+++ src/viewGif.ts
@@ -14,12 +14,32 @@ const viewWebViewHtml: (imageHtml: string) => string = (
 <body>
     <div><img src="https://giflens.org/assets/PoweredBy_200_Horizontal_Light-Backgrounds_With_Logo.gif" alt="powered by giphy"/></div>
     ${imageHtml}
+    <script>
+      // acquiring the vscode webview specific api to send messages to the extension
+      const vscode = acquireVsCodeApi();
+
+      // event handler that sends back to the extension the url of the clicked gif
+      const sendGif = event => {
+        const url = event.target.getAttribute('src');
+        const label = event.target.getAttribute('alt');
+        vscode.postMessage({
+          command: 'url',
+          text: { url, label},
+        });
+      };
+
+      const gifItem = document.getElementById("gif");
+      gifItem.addEventListener('click', sendGif);
+    </script>
 </body>
 </html>`;
 
 const createGifHtml: (gif: HistoryEntry | FavoritesEntry) => string = (
 	gif: HistoryEntry | FavoritesEntry
-) => `<img class="search-img" src="${gif.gifUri}" alt="${gif.label}" />`;
+) =>
+	`<img id="gif" style="cursor: pointer;" src="${gif.gifUri}" alt="${
+		gif.label
+	}" />`;
 
 export const viewGif: (
 	gif: HistoryEntry | FavoritesEntry,
@@ -33,6 +53,7 @@ export const viewGif: (
 	const existingPanel: vscode.WebviewPanel | undefined = workspaceState.get(
 		'viewPanel'
 	);
+	const editor: vscode.TextEditor | undefined = vscode.window.activeTextEditor;
 	if (existingPanel) {
 		existingPanel.webview.html = viewWebViewHtml(createGifHtml(gif));
 		existingPanel.title = gif.label;
@@ -40,9 +61,27 @@ export const viewGif: (
 		const panel: vscode.WebviewPanel = vscode.window.createWebviewPanel(
 			'gifView', // Identifies the type of the webview. Used internally
 			gif.label, // Title of the panel displayed to the user
-			vscode.ViewColumn.Beside // Editor column to show the new webview panel in.
+			vscode.ViewColumn.Beside, // Editor column to show the new webview panel in.
+			{
+				enableScripts: true,
+			}
 		);
 		panel.webview.html = viewWebViewHtml(createGifHtml(gif));
+
+		// when the image is clicked, create a GIFLENS tag and close the webview
+		panel.webview.onDidReceiveMessage(message => {
+			panel.dispose();
+			if (editor) {
+				vscode.commands.executeCommand(
+					'giflens.addGif',
+					message.text.url,
+					editor
+				);
+			} else {
+				vscode.window.showInformationMessage('You had no active editor');
+			}
+		});
+
 		// when the panel is closed, the state is reinitialized
 		panel.onDidDispose(() => {
 			// using undefined to remove a key from a Memento
